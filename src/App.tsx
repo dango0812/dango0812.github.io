8e8b421a7fcd756ca351ee85a2a2eca5cf106692
dango0812 <donggyu@DESKTOP-BRGTDTS.localdomain>
2023-12-04 23:40:28 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+
+  const charAt = `
+    ꒰ ˶• ༝ •˶꒱
+    ./づ~ ♡
+  `;
+
+  console.info(charAt)
+
+  return (
+    <h1>
+      Simple React Typescript Tailwind Sample
+    </h1>
+  );
+}
+
+export default App;
