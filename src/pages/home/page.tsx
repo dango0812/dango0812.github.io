6f77afc9469eb9ae95bf8b166042fb6bd4afbaae
dango0812 <donggyu@DESKTOP-BRGTDTS.localdomain>
2023-12-06 00:19:07 +0900

    design(navbar): Added Home nav-bar components

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,10 +1,12 @@
 // sections
 import HomeHero from "src/sections/home/home-hero"
+import HomeNavbar from "src/sections/home/home-navbar"
 
 export default function HomePage() {
     return (
         <main className="bg-white overflow-hidden">
             <HomeHero />
+            <HomeNavbar />
         </main>
     );
 }
