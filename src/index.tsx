989bb71bd06c3070f474a5a9ef0ea6e015a02c6e
dango0812 <ehdrbsla12000@gmail.com>
2023-12-07 00:38:45 +0900

    feat(msw): Added msw handlers & setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { worker } from 'src/_mock/msw/msw-setup';
 
 import './index.css';
 import App from './App';
+
+if (process.env.NODE_ENV === 'development') worker.start();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
