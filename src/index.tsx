2c8bc09d82eff8085e6e58f3fd542eadf62f67a7
dango0812 <ehdrbsla12000@gmail.com>
2023-12-07 14:36:01 +0900

    remove(msw): Deleted msw (Not available at build), so use  my-json-server

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
 import { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { worker } from 'src/_mock/msw/msw-setup';
 
 import './index.css';
 import App from './App';
-
-if (process.env.NODE_ENV === 'development') worker.start();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
