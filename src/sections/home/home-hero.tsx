fed52c7a817cac58b780b8a65f7994ab647a5e8b
dango0812 <ehdrbsla12000@gmail.com>
2023-12-12 13:16:17 +0900

    refactor(hero-typo): Modified hero section typeography

--- src/sections/home/home-hero.tsx
+++ src/sections/home/home-hero.tsx
@@ -3,13 +3,13 @@ import { memo } from 'react'
 
 function HomeHero() {
     return (
-        <section className="max-w-screen-lg mx-auto pt-32">
+        <section className="max-w-screen-lg mx-auto pt-16">
             <div className="w-full flex justify-center items-center flex-col gap-1">
                 <img className="rounded-full w-32 h-32" src="/assets/profile.jpg" alt="profile"/>
                 <h1 className="font-bold text-2xl">
-                    Dango0812
+                    김동규
                 </h1>
-                <h2 className="text-base whitespace-nowrap">Frontend Developer 🫠</h2>
+                <h2 className="text-base whitespace-nowrap">Web Frontend Developer 🫠</h2>
             </div>
         </section>
     )
