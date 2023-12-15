1b4168c2f55768528b42910ce166735e53bc0c13
dango0812 <ehdrbsla12000@gmail.com>
2023-12-15 17:38:35 +0900

    refactor(home-hero): Modified home hero section (use html tag -> components)

--- src/sections/home/home-hero.tsx
+++ src/sections/home/home-hero.tsx
@@ -1,17 +1,25 @@
 // react
 import { memo } from 'react'
+// components
+import Typography from 'src/components/typography';
+import Stack from 'src/components/stack';
+import Container from 'src/components/container';
 
 function HomeHero() {
     return (
-        <section className="max-w-screen-lg mx-auto pt-16">
-            <div className="w-full flex justify-center items-center flex-col gap-1">
+        <Container size='lg' className='pt-16'>
+            <Stack direction='horizontal' align='center' justify='center' spacing={16}>
                 <img className="rounded-full w-32 h-32" src="/assets/profile.jpg" alt="profile"/>
-                <h1 className="font-bold text-2xl">
-                    김동규
-                </h1>
-                <h2 className="text-base whitespace-nowrap">Web Frontend Developer 🫠</h2>
-            </div>
-        </section>
+                <Stack direction='horizontal' spacing={4}>
+                    <Typography variant='h6'>
+                        김동규
+                    </Typography>
+                    <Typography variant='body1' className='font-normal'>
+                        Web Frontend Developer 🫠
+                    </Typography>
+                </Stack>
+            </Stack>
+        </Container>
     )
 }
 
