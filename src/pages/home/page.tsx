3643b7c3782284fe6aec21624aa91929e0809def
dango0812 <ehdrbsla12000@gmail.com>
2023-12-15 19:21:32 +0900

    feat(tabs): Added tab click event (if tab click, change tab view)

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -2,10 +2,11 @@
 import HomeHero from "src/sections/home/home-hero"
 // hooks
 import useTabs from 'src/hooks/useTabs'
+// view
 import { ResumeView } from "src/sections/home/tab/resume/view";
 
 export default function HomePage() {
-    const { currentTab } = useTabs('Resume')
+    const { currentTab, onChangeTab } = useTabs('Resume')
 
     const HOME_TABS = [
         {
@@ -13,16 +14,12 @@ export default function HomePage() {
             component: <ResumeView />
         },
         {
-            value: 'Experience',
-            component: <>tab1</>
-        },
-        {
             value: 'Projects',
-            component: <>tab2</>
+            component: <>Projects</>
         },
         {
-            value: 'Links',
-            component: <>tab2</>
+            value: 'StudyLog',
+            component: <>StudyLog</>
         },
     ];
 
@@ -30,9 +27,11 @@ export default function HomePage() {
         <main className="bg-white">
             <HomeHero />
             <section className="border-b border-gray-200 text-sm font-medium text-center pt-8">
-                <ul className="-mb-px flex justify-center items-center">
+                <ul className="-mb-px flex justify-center items-center" role="tablist">
                     {HOME_TABS.map((tab) => (
-                        <li key={tab.value} className="me-2 cursor-pointer">
+                        // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/click-events-have-key-events.md
+                        <li key={tab.value} value={tab.value} className="me-2 cursor-pointer" role="tab" aria-selected={currentTab === tab.value}
+                        onClick={() => onChangeTab(tab.value)} onKeyUp={() => null}>
                             <h3 className={`inline-block px-4 py-2 mb-1 rounded-lg
                                     ${tab.value === currentTab ? 'text-orange-500' : 'text-zinc-500'}
                                     ${tab.value === currentTab && 'font-bold'}
@@ -49,7 +48,7 @@ export default function HomePage() {
 
             {HOME_TABS.map((tab) => {
                 const isMatched = tab.value === currentTab;
-                return isMatched && <section key={tab.value} className="max-w-screen-lg mx-auto py-6">{tab.component}</section>
+                return isMatched && <section key={tab.value} className="max-w-screen-lg mx-auto py-6" role="tabpanel">{tab.component}</section>
             })}
         </main>
     );
