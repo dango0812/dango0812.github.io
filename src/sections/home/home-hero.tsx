91638013d58bb3b3abb6bde0eb88cf7400e9b51e
dango0812 <ehdrbsla12000@gmail.com>
2023-12-29 02:09:51 +0900

    design(hero): Modified hero section (left image & right name, subtitle, contact & channel)

--- src/sections/home/home-hero.tsx
+++ src/sections/home/home-hero.tsx
@@ -6,20 +6,56 @@ import Stack from 'src/components/stack';
 import Container from 'src/components/container';
 
 function HomeHero() {
+
+    const CONTACT_CHANNEL_INFORMATION = [
+        {
+            icon: '✉️',
+            value: 'ehdrbsla12000@gmail.com',
+            link: 'mailto:ehdrbsla12000@gmail.com'
+        },
+        {
+            icon: '🧑‍💻',
+            value: 'Github',
+            link: 'https://github.com/dango0812'
+        },
+        {
+            icon: '📚',
+            value: 'Study Log',
+            link: 'https://github.com/dango0812/study-log'
+        },
+    ];
+
     return (
-        <Container size='lg' className='pt-16'>
-            <Stack direction='horizontal' align='center' justify='center' spacing={16}>
-                <img className="rounded-full w-32 h-32" src="/assets/profile.jpg" alt="profile"/>
-                <Stack direction='horizontal' spacing={4}>
-                    <Typography variant='h6'>
-                        김동규
-                    </Typography>
-                    <Typography variant='body1' className='font-normal'>
-                        Web Frontend Developer 🫠
-                    </Typography>
-                </Stack>
-            </Stack>
-        </Container>
+        <section>
+            <Container size='lg'>
+                <div className='grid grid-flow-row md:grid-cols-2 gap-8'>
+                    <div className='flex justify-center items-center'>
+                        <img className="h-48 md:h-72" src="/assets/profile.jpg" alt="profile"/>
+                    </div>
+                    <div className='flex flex-col justify-center gap-8'>
+                        <Stack direction='horizontal' className='items-center md:items-start' spacing={8}>
+                            <Typography variant='h4' className='dark:text-white'>
+                                김 동 규
+                            </Typography>
+                            <Typography variant='h6' className='font-normal dark:text-white'>
+                                Web Frontend Developer 🫠
+                            </Typography>
+                        </Stack>
+                        <Stack direction='horizontal' justify='start' align='start' spacing={8}>
+                            <hr className="w-full h-px border bg-red-500 dark:bg-gray-500" />
+                            {CONTACT_CHANNEL_INFORMATION.map((info) => (
+                                <a className='max-w-max md:hover:scale-110 transition-transform ease-in-out duration-300' href={info.link} target='_blank' rel="noreferrer noopener">
+                                    <Typography variant='subtitle1' className='dark:text-white'>
+                                        {info.icon} {info.value}
+                                    </Typography>
+                                </a>
+                            ))}
+                            <hr className="w-full h-px border bg-red-500 dark:bg-gray-500" />
+                        </Stack>
+                    </div>
+                </div>
+            </Container>
+        </section>
     )
 }
 
