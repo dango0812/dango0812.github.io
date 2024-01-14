a46e64cce67a455f7300cac609bab824baabc66e
dango0812 <ehdrbsla12000@gmail.com>
2024-01-15 00:34:57 +0900

    design(layout): Comment header theme-toggle layout & Added layout padding 4rem->64px

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,6 +1,6 @@
 // components
-import Container from 'src/components/container'
-import ThemeToggle from 'src/components/theme-toggle'
+// import Container from 'src/components/container'
+// import ThemeToggle from 'src/components/theme-toggle'
 // sections
 import HomeAwards from 'src/sections/home/home-awards'
 import HomeBlogPosts from 'src/sections/home/home-blog-posts'
@@ -9,22 +9,24 @@ import HomeIntroduce from 'src/sections/home/home-introduce'
 import HomeProjects from 'src/sections/home/home-projects'
 import HomeTechStack from 'src/sections/home/home-tech-stack'
 // useTheme
-import { useThemeContext } from 'src/theme/theme-context'
+// import { useThemeContext } from 'src/theme/theme-context'
 
 const rootStyle = 'bg-gradient-to-r from-blue-100 to-blue-100 dark:bg-none dark:bg-[#161c24] min-h-screen p-6';
 
 export default function HomePage() {
 
-    const { currentTheme, changeTheme } = useThemeContext();
+    // const { currentTheme, changeTheme } = useThemeContext();
 
     return (
         <main className={rootStyle}>
+            {/*
             <header>
                 <Container size='md' className='h-20 flex items-center justify-end'>
                     <ThemeToggle themeColor={currentTheme || 'dark'} handleThemeColor={changeTheme}/>
                 </Container>
             </header>
-            <article className='flex flex-col justify-center gap-14 md:gap-16'>
+            */}
+            <article className='flex flex-col justify-center gap-14 md:gap-16 py-16'>
                 <HomeHero />
                 <HomeIntroduce />
                 <HomeTechStack />
