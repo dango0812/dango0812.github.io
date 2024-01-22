8df760f419dade3089ed038d519eb52973c781d9
dango0812 <ehdrbsla12000@gmail.com>
2024-01-22 23:59:34 +0900

    design(home-introduce): Modified home introduce section typography  ~다 -> ~요

--- src/sections/home/home-introduce.tsx
+++ src/sections/home/home-introduce.tsx
@@ -15,10 +15,10 @@ function HomeIntroduce() {
                 <hr className="w-full h-px border bg-red-500 dark:bg-gray-500" />
                 <Card>
                     <Typography variant='subtitle2' className='whitespace-pre-line font-normal leading-8 dark:text-white'>
-                        {`안녕하세요 ! 끊임없는 성장을 추구하는 개발자 김동규입니다.
-                            👨‍👩‍👧‍👦 팀에 소속되어 함께 일하며 서로의 아이디어와 관점을 공유하며 성장하는 것을 좋아합니다.
-                            🏋️‍♂️ 가독성 좋은 코드를 작성하기 위해 노력하고 있습니다.
-                            🎖️ 기술적인 측면과 아울러 사용자 시각에서의 UI/UX를 개선하는데 관심이 많습니다.
+                        {`안녕하세요 👋 끊임없는 성장을 추구하는 개발자 김동규예요.
+                            👨‍👩‍👧‍👦 팀에 소속되어 함께 일하며 서로의 아이디어와 관점을 공유하며 성장하는 것을 좋아해요.
+                            🏋️‍♂️ 가독성 좋은 코드를 작성하기 위해 노력해요.
+                            🎖️ 기술적인 측면과 아울러 사용자 시각에서의 UI/UX를 개선하는데 관심이 많아요.
                         `}
                     </Typography>
                 </Card>
