bf1d2c2839a38a47f7041f7e817c27d2580c7824
dango0812 <ehdrbsla12000@gmail.com>
2024-01-29 00:34:11 +0900

    refactor(blog-posts): Modified postItem type separation & blog api endpoint

--- src/sections/home/home-blog-posts.tsx
+++ src/sections/home/home-blog-posts.tsx
@@ -8,54 +8,21 @@ import Stack from 'src/components/stack';
 import Typography from 'src/components/typography';
 // utils
 import { fUtcToKstDate } from 'src/utils/format-time';
-
-type postItem = {
-    "type_of": string;
-    "id": number;
-    "title": string;
-    "description": string;
-    "readable_publish_date": string;
-    "slug": string;
-    "path": string;
-    "url": string;
-    "comments_count": number;
-    "public_reactions_count": number;
-    "collection_id": number;
-    "published_timestamp": string;
-    "positive_reactions_count": number;
-    "cover_image": string;
-    "social_image": string;
-    "canonical_url": string;
-    "created_at": string;
-    "edited_at": string;
-    "crossposted_at": string;
-    "published_at": string;
-    "last_comment_at": string;
-    "reading_time_minutes": number;
-    "tag_list": string[];
-    "tags": string;
-    "user": {
-        "name": string;
-        "username": string;
-        "twitter_username": string;
-        "github_username": string;
-        "user_id": number;
-        "website_url": string;
-        "profile_image": string;
-        "profile_image_90": string;
-    }
-}
+// types
+import { PostItem } from 'src/types/blog';
+// config
+import { BLOG_API } from 'src/config';
 
 function HomeBlogPosts() {
     const { data, isLoading, isFetching, error } = useQuery({
-        queryKey: ['id'],
+        queryKey: ['blogPosts'],
         queryFn: () => getBlogPosts(),
         refetchOnWindowFocus: false
     });
 
     const memoizedBlogPosts = useMemo(
         () => ({
-            data: isLoading ? [] : data as Array<postItem>,
+            data: isLoading ? [] : data as Array<PostItem>,
             loading: isLoading,
             fetching: isFetching,
             error: error || null,
@@ -65,8 +32,8 @@ function HomeBlogPosts() {
     );
 
     const getBlogPosts = async() => {
-        const endpoint = 'https://dev.to/api/articles?username=dango0812&api_key=fDwtyzQ7BU8gb9MEjzt3CB4a';
-        const response = await fetch(endpoint);
+        const endPoint = `${BLOG_API.articles}?username=${BLOG_API.username}&api_key=${BLOG_API.apiKey}`;
+        const response = await fetch(endPoint);
         if (!response.ok) {
             throw new Error('Network response was not ok.')
         }
@@ -83,11 +50,11 @@ function HomeBlogPosts() {
                 </Typography>
                 <hr className="w-full h-px bg-slate-500" />
                     <Stack direction='horizontal' align='start' justify='start' spacing={16}>
-                        {memoizedBlogPosts.data.map((post: postItem) => (
-                            <a key={post.id} href="#">
+                        {memoizedBlogPosts.data.map((post: PostItem) => (
+                            <a key={post.id} href={post.url} target='_blank' rel='noreferrer noopener'>
                                 <Card className='px-0 py-0 md:flex-row min-h-[160px] border-none hover:bg-transparent dark:md:hover:bg-gray-700'>
                                     <Stack direction='horizontal' align='start' justify='start' className='md:flex md:flex-row' spacing={4}>
-                                        <img className="w-full md:w-56 h-48 object-cover rounded-t-lg md:rounded-l-lg md:rounded-r-none" src={post.cover_image} alt="blog cover" />
+                                        <img className="w-full md:w-56 md:h-56 rounded-t-lg md:rounded-l-lg md:rounded-r-none" src={post.cover_image} alt="blog cover" />
                                         <Stack direction='horizontal' align='start' justify='start' spacing={16} className='p-4'>
                                             <Typography variant='h6' color='black' className='font-bold  dark:text-white'>
                                                 {post.title}
