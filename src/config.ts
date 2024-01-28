bf372834a203d7ce7dddec7a0c42b9efd1dfc895
dango0812 <ehdrbsla12000@gmail.com>
2024-01-29 00:32:32 +0900

    chore(config): Added config global file (use env file data)

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const BLOG_API = {
+    username: process.env.REACT_APP_BLOG_API_USERNAME,
+    apiKey: process.env.REACT_APP_BLOG_API_KEY,
+    articles: `${process.env.REACT_APP_BLOG_API}/articles`
+}
