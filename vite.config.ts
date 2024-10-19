0375a3ee5376968ccecb2caa90b96a5c65c3edf9
dango0812 <ehdrbsla12000@gmail.com>
2024-10-20 04:04:18 +0900

    chore(vite): legacy targets config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,16 @@ import { defineConfig } from "vite";
 // vite - plugins
 import react from "@vitejs/plugin-react";
 import pathAlias from "vite-tsconfig-paths";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pathAlias()],
+  plugins: [
+    react(),
+    pathAlias(),
+    legacy({
+      targets: ['chrome >= 64', 'safari >= 12'],
+      modernPolyfills: true
+    })
+  ],
 })
