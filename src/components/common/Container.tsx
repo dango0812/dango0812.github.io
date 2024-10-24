8019f898234f555b97ab892c49146ea1bf6664bd
dango0812 <ehdrbsla12000@gmail.com>
2024-10-24 11:39:58 +0900

    feat: add container components

---
+++ src/components/common/Container.tsx
@@ -0,0 +1,12 @@
+// emotion
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+    position: relative;
+    width: 100%;
+    max-width: 1200px;
+    margin: 0px auto;
+    padding: 0px 24px;
+`;
+
+export default Container;
