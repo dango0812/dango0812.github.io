477b1e57a963175ccaeedcb70fe941d8f4c26e53
dango0812 <ehdrbsla12000@gmail.com>
2024-10-30 14:31:06 +0900

    refactor: modified animation fade in components

--- src/components/animation/AnimationFadeIn.tsx
+++ src/components/animation/AnimationFadeIn.tsx
@@ -1,36 +1,45 @@
-// react
-import * as React from "react";
 // emotion
 import styled from "@emotion/styled";
 
-interface AnimationFadeInProps {
+interface AnimationFadeInProps extends React.HTMLAttributes<HTMLDivElement>{
     time: number;
     htmlTag?: keyof JSX.IntrinsicElements;
     children: React.ReactNode;
-}
-
-const AnimationFadeIn = ({ time, htmlTag, children }: AnimationFadeInProps) => {
-    const RootStyle = styled((htmlTag || "div") as keyof JSX.IntrinsicElements)<{ time: number }>`
-        animation: fadeIn ${({ time }) => `${time}s`};
-        -moz-animation: fadeIn ${({ time }) => `${time}s`}; /* Firefox */
-        -webkit-animation: fadeIn ${({ time }) => `${time}s`}; /* Safari and Chrome */
-        -o-animation: fadeIn ${({ time }) => `${time}s`}; /* Opera */
-
-        @keyframes fadeIn {
-            from {
-                opacity: 0;
-            }
-            to {
-                opacity: 1;
-            }
-        }
-    `;
-
-    return (
-        <RootStyle time={time}>
-            {children}
-        </RootStyle>
-    );
 };
 
-export default AnimationFadeIn;
+const Wrapper = styled.div<{
+    time: number;
+}>`
+    display: inline-block;
+    animation: fadeIn ${({ time }) => `${time}s`};
+    -moz-animation: fadeIn ${({ time }) => `${time}s`}; /* Firefox */
+    -webkit-animation: fadeIn ${({ time }) => `${time}s`}; /* Safari and Chrome */
+    -o-animation: fadeIn ${({ time }) => `${time}s`}; /* Opera */
+    
+    @keyframes fadeIn {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+`;
+
+
+export default function AnimationFadeIn({
+    time,
+    htmlTag,
+    children,
+    ...rest
+}: AnimationFadeInProps) {
+    return (
+        <Wrapper
+            time={time}
+            as={htmlTag}
+            {...rest}
+        >
+            {children}
+        </Wrapper>
+    )
+}
