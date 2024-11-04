b8a4cef3d9f35f9103c3a2dc246ddda70530cadb
dango0812 <ehdrbsla12000@gmail.com>
2024-11-05 00:59:39 +0900

    feat: container component height

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -4,6 +4,7 @@ import styled from "@emotion/styled";
 const Container = styled.div`
     position: relative;
     width: 100%;
+    height: 100%;
     max-width: 800px;
     margin: 0px auto;
     padding: 0px 24px;
