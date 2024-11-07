4cddd11f7ae423784428e16ccbf159e62ff43c5f
dango0812 <ehdrbsla12000@gmail.com>
2024-11-07 16:25:55 +0900

    feat: added react-query provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 // react
 import { createRoot } from "react-dom/client";
-
+// react-query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // providers
 import RouterProvider from "@providers/RouterProvider";
-
 // components
 import GlobalStyles from "@/components/common/GlobalStyles";
 
+const queryClient = new QueryClient();
 const root = createRoot(document.getElementById("root") as HTMLElement);
-
 root.render(
   <>
     <GlobalStyles />
-    <RouterProvider />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider />
+    </QueryClientProvider>
   </>
 )
