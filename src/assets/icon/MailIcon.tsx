8b62a9d87df4f3f5d6e2498cfc11c2fa39d1256e
dango0812 <ehdrbsla12000@gmail.com>
2024-12-23 17:07:34 +0900

    design: modified icon style

--- src/assets/icon/MailIcon.tsx
+++ src/assets/icon/MailIcon.tsx
@@ -4,14 +4,21 @@ const MailIcon = ({ ...props }) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24px"
-            height="24px"
+            width="24"
+            height="24"
             viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.7"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             {...props}
         >
             <path
-                fill="currentColor"
-                d="M4 20q-.825 0-1.412-.587T2 18V6q0-.825.588-1.412T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.587 1.413T20 20zm8-7l8-5V6l-8 5l-8-5v2z"
+                d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"
+            />
+            <path
+                d="M3 7l9 6l9 -6"
             />
         </svg>
     )
