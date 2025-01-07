cf59a81466b66bf3555dc533f2a0f7d9cff97beb
dango0812 <ehdrbsla12000@gmail.com>
2025-01-07 17:18:46 +0900

    design: modified container component & added grid, infolist component

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,16 +1,18 @@
 // emotion
 import styled from "@emotion/styled";
+// constants
+import { mediaQuery } from "@constants/mediaQuery";
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-
     width: 100%;
     height: 100%;
     max-width: var(--container-size);
-    
+    flex-grow: 1;
     margin: 0px auto;
-    padding: 0px 24px;
+    padding: 0px 20px;
+    ${mediaQuery.laptop} {
+        padding: 0px;
+    }
 `;
 
 export default Container;
