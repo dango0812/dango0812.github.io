6aa5f6aaa2f6e817ca435c0ff364b3e7ea88a622
dango0812 <ehdrbsla12000@gmail.com>
2025-02-14 11:14:17 +0900

    feat: vite & gh-pages static build config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     react({
       jsxImportSource: "@emotion/react"
@@ -16,5 +17,5 @@ export default defineConfig({
       targets: ['chrome >= 64', 'safari >= 12'],
       modernPolyfills: true
     })
-  ]
+  ],
 })
