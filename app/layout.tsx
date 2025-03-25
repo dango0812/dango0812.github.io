f0a0afa7d7cfb4f0141c07a46490715c3901cc8d
dango0812 <ehdrbsla12000@gmail.com>
2025-03-25 15:12:53 +0900

    feat: RootLayout head meta 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,36 @@
+// next
 import type { Metadata } from "next";
+// mocks
+import { _MetaData } from "@_mocks/layout";
+// styles
+import "@styles/globals.css";
+
+export const metadata: Metadata = {
+  title: _MetaData.title,
+  description: _MetaData.description
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+  children
+}: RootLayoutProps) {
   return (
     <html lang="ko">
+      <head>
+        {/* author */}
+        <meta name="author" content={_MetaData.author} />
+
+        {/* seo */}
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="googlebot" content="noindex, nofollow" />
+
+        {/* font */}
+        <link rel="stylesheet" as="style" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard-dynamic-subset.min.css" />
+      </head>
+
       <body>
         {children}
       </body>
