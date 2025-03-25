1912308c4b42c2fcc9d79a466bd5ff354a65a1ae
dango0812 <ehdrbsla12000@gmail.com>
2025-03-25 15:13:59 +0900

    feat: Container 컴포넌트 추가

--- app/components/ui/Container.tsx
+++ app/components/ui/Container.tsx
@@ -1,21 +1,20 @@
 // cva
 import { cva, VariantProps } from "class-variance-authority";
-// cn
+// libs
 import { cn } from "@libs/tailwind";
 
 export const containerVariants = cva(
     [
-        "w-full",
-        "flex items-start justify-stretch"
+        "w-full flex items-start justify-stretch"
     ],
     {
         variants: {
             maxWidth: {
-                xs: "max-w-2xl",
-                sm: "max-w-3xl",
-                md: "max-w-4xl",
-                lg: "max-w-5xl",
-                xl: "max-w-7xl",
+                xs: "max-w-2xl mx-auto",
+                sm: "max-w-3xl mx-auto",
+                md: "max-w-4xl mx-auto",
+                lg: "max-w-5xl mx-auto",
+                xl: "max-w-7xl mx-auto",
                 fullWidth: "max-w-full",
                 auto: "container"
             }
