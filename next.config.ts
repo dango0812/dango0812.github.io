b032ea34459dcde020d1205c56b9c1b48c54797c
dango0812 <ehdrbsla12000@gmail.com>
2025-03-27 00:27:44 +0900

    chore: image remote dev.to media host 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      // dev.to media url
+      {
+        protocol: "https",
+        hostname: "media2.dev.to"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
