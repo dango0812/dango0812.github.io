f48a0b66f8151cae0693c2628443222f0ddd77a6
dango0812 <ehdrbsla12000@gmail.com>
2025-03-29 01:06:10 +0900

    refactor: 날짜 포멧 yyyy.mm.dd 형식으로 변경

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -12,5 +12,5 @@ export const formatDateTime = (datetime: string | Date) => {
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
 
-    return `${year}년 ${month}월 ${day}일`;
+    return `${year}.${month}.${day}`;
 }
