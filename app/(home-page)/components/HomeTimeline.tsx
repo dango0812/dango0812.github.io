90ab4cf304e17957ac1d738cecd80200cd245f55
dango0812 <ehdrbsla12000@gmail.com>
2025-03-30 23:56:09 +0900

    design: time line section 전체적인 디자인 수정

--- app/(home-page)/components/HomeTimeline.tsx
+++ app/(home-page)/components/HomeTimeline.tsx
@@ -5,11 +5,11 @@ import { _HomeTimelineSection } from "@_mocks/home";
 // components
 import { Container, ColumnFlex, Typography, RowFlex } from "@components/ui";
 
-export default function HomeTimeline() {
+export default function HomeTimelineSection() {
     return (
         <section
             id="timeline"
-            className="py-10 px-5 xl:px-0 "
+            className="py-15 px-5 xl:px-0 "
         >
             <Container
                 maxWidth="lg"
@@ -41,45 +41,40 @@ export default function HomeTimeline() {
                         </Typography>
                     </ColumnFlex>
 
-                    <div
-                        className="grid grid-cols-1 md:grid-cols-2 gap-10 w-full"
+                    <Container
+                        maxWidth="sm"
                     >
-                        {_HomeTimelineSection.details.map(({ title, iconSrc, items }, idx) => (
-                            <div
-                                key={idx}
-                                className="col-span-1 shadow-md rounded-xl bg-white p-5 border border-gray-200"
-                            >
+                        <RowFlex
+                            justifyContent="space-between"
+                            className="w-full flex-wrap gap-10 md:gap-20"
+                        >
+                            {_HomeTimelineSection.details.map(({ title, iconSrc, items }) => (
                                 <ColumnFlex
-                                    alignItems="center"
-                                    className="text-center gap-7.5"
+                                    className="w-full max-w-80 gap-2.5"
+                                    key={title}
                                 >
-                                    <ColumnFlex
-                                        alignItems="center"
-                                        justifyContent="center"
-                                        className="gap-2"
+                                    <Image
+                                        src={iconSrc}
+                                        width={40}
+                                        height={40}
+                                        alt={title}
+                                    />
+                                    <Typography
+                                        as="h3"
+                                        fontSize="xl"
+                                        fontWeight={700}
+                                        className="px-0.5"
                                     >
-                                        <Image
-                                            src={iconSrc}
-                                            width={40}
-                                            height={40}
-                                            alt="icon-link"
-                                        />
-                                        <Typography
-                                            as="h3"
-                                            fontSize="xl"
-                                            fontWeight={700}
-                                        >
-                                            {title}
-                                        </Typography>
-                                    </ColumnFlex>
+                                        {title}
+                                    </Typography>
 
                                     <ColumnFlex
-                                        className="gap-3.5"
+                                        className="gap-1"
                                     >
-                                        {items.map(({ icon, title, date }) => (
+                                        {items.map(({ icon, title }) => (
                                             <RowFlex
                                                 key={title}
-                                                className="gap-2"
+                                                className="gap-1.5"
                                             >
                                                 <Typography
                                                     as="span"
@@ -87,31 +82,18 @@ export default function HomeTimeline() {
                                                     {icon}
                                                 </Typography>
 
-                                                <ColumnFlex
-                                                    className="text-start gap-3 lg:flex-row lg:justify-between lg:items-center"
+                                                <Typography
+                                                    as="h4"
                                                 >
-                                                    <Typography
-                                                        as="h4"
-                                                    >
-                                                        {title}
-                                                    </Typography>
-                                                    <Typography
-                                                        as="p"
-                                                        fontSize="sm"
-                                                        color="gray"
-                                                        fontWeight={600}
-                                                        whiteSpace="nowrap"
-                                                    >
-                                                        {date}
-                                                    </Typography>
-                                                </ColumnFlex>
+                                                    {title}
+                                                </Typography>
                                             </RowFlex>
                                         ))}
                                     </ColumnFlex>
                                 </ColumnFlex>
-                            </div>
-                        ))}
-                    </div>
+                            ))}
+                        </RowFlex>
+                    </Container>
                 </ColumnFlex>
             </Container>
         </section>
